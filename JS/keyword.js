1d1998f7c7020ee1e6221718dcd7ed6f25eaa27d
NyiLatt35 <nyilatt.3520@gmail.com>
2022-06-07 13:32:48 +0630

    7 June 2022

---
+++ JS/keyword.js
@@ -0,0 +1,30 @@
+// console.log("Hello World!");
+// console.warn("hi");
+// console.log(window);
+// console.log(window.document);
+// console.log(document);
+// window.alert("Hello, Javascript");
+// alert("Hi");
+
+// Let => no redeclare / allow reassign
+let name; // declare
+name="Hello"; // assign
+console.log(name);
+// let name; // redeclare
+name="Mg Mg"; // reassign
+console.log(name);
+
+// Var => allown redeclare and reassign
+var age; // declare
+age=12; // assign
+console.log(age);
+var age; // redeclare
+age=33; // reassign
+console.log(age);
+
+// Const => no redeclare and reassign
+const y=12;
+console.log(y);
+// y=22; // reassign 
+// console.log(y);
+// const y=5;
