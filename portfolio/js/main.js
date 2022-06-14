83e18c1870526c95b79627ef486332c0ba61edc8
NyiLatt35 <nyilatt.3520@gmail.com>
2022-06-14 09:30:55 +0630

    14 June 2022

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -8,10 +8,31 @@ toggle.addEventListener("click", function () {
 });
 
 const nav = document.querySelector('nav');
+const up = document.querySelector('#up');
+
 window.addEventListener("scroll", function () {
     if (window.scrollY >= 650) {
         nav.style.backgroundColor = "#ddd";
     } else {
         nav.style.backgroundColor = "";
     }
+    // if(window.scrollY >=850){
+    //     up.classList.add("active");
+    // }else {
+    //     up.classList.remove('active');
+    // }
+
+    // if(window.scrollY >= 850) up.classList.add('active');
+    // else up.classList.remove('active');
+
+    // condition ? ture : false
+    window.scrollY >= 850 ? up.classList.add('active') : up.classList.remove('active');
+
 })
+
+up.onclick=()=>{
+    window.scrollTo({
+        behavior: "smooth",
+        top: 0,
+    })
+}
